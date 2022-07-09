b99101afd1bf94216db7db084db81d251ee2ded6
= <eechemane29@gmail.com>
2022-07-09 19:37:30 +0800

    launcher icons added

---
+++ leafjem_app/lib/pallete.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const MaterialColor mcgpalette0 =
+      MaterialColor(_mcgpalette0PrimaryValue, <int, Color>{
+    50: Color(0xFFEDE9E7),
+    100: Color(0xFFD2C8C2),
+    200: Color(0xFFB5A49A),
+    300: Color(0xFF977F72),
+    400: Color(0xFF806353),
+    500: Color(_mcgpalette0PrimaryValue),
+    600: Color(0xFF624130),
+    700: Color(0xFF573828),
+    800: Color(0xFF4D3022),
+    900: Color(0xFF3C2116),
+  });
+  static const int _mcgpalette0PrimaryValue = 0xFF6A4835;
+
+  static const MaterialColor mcgpalette0Accent =
+      MaterialColor(_mcgpalette0AccentValue, <int, Color>{
+    100: Color(0xFFFF9F7B),
+    200: Color(_mcgpalette0AccentValue),
+    400: Color(0xFFFF5515),
+    700: Color(0xFFFB4400),
+  });
+  static const int _mcgpalette0AccentValue = 0xFFFF7A48;
+}
